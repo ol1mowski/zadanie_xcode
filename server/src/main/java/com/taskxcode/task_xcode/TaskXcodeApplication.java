1858c344bb76a2eae4461063e490b39f2ee1bf62
Ol1mowski <olimarki123@wp.pl>
2025-10-07 16:05:37 +0200

    feat: Implement initial project structure and setup

---
+++ server/src/main/java/com/taskxcode/task_xcode/TaskXcodeApplication.java
@@ -0,0 +1,13 @@
+package com.taskxcode.task_xcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskXcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskXcodeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/taskxcode/task_xcode/TaskXcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskxcode.task_xcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskXcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
